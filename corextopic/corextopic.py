0f1c66c2c3639d09734832dc54236578b0360b35
ferdiwo <38081667+ferdiwo@users.noreply.github.com>
2019-08-15 15:51:44 -0400

    Update corextopic.py

--- corextopic/corextopic.py
+++ corextopic/corextopic.py
@@ -22,7 +22,7 @@ import warnings
 import numpy as np  # Tested with 1.8.0
 from os import makedirs
 from os import path
-from scipy.misc import logsumexp  # Tested with 0.13.0
+from scipy.special import logsumexp  # Tested with 0.13.0
 import scipy.sparse as ss
 from six import string_types # For Python 2&3 compatible string checking
 from sklearn.externals import joblib
